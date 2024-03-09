48345a5ecf8b8c02820a5bf00c3c9c858714b89f
EStherMmutisya <starjacob994@gmail.com>
2024-03-09 22:06:01 +0300

    completed assignment

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,58 @@
 from vehicle import Vehicle
 
-class Car(Vehicle):
-    pass
+# class Car(Vehicle):
+
+    # def __init__(self, wheel_Size,wheel_number): 
+    #     self.wheel_Size = wheel_Size
+    #     self.wheel_number = wheel_number
+
+    #     def go(self):
+    #         return "vrrrrrrooom!"
+
+
+
+class  Car(Vehicle):
+    def __init__(self, wheel_size, wheel_number):
+            self.wheel_size = wheel_size
+            self.wheel_number = wheel_number
+
+    def go(self):
+            return "vrrrrrrrooom!"
+
+    def fill_up_tank(self):
+            return "filling up!"
+
+
+    #     def fill_up_tank(self):
+    #         return "filling up!"
+
+        # def __init__(self,wheel_number):
+        #     self.wheel_number = wheel_number
+        #     self.wheel_Size = wheel_number
+
+        # def go(self):
+        #     return "vrrrrrrrooom!"
+
+        # def fill_up_tank (self):
+        #     return "filling up!"
+
+    # def __init__(self, wheel_number, wheel_size):
+    #     self.wheel_number = wheel_number
+    #     self.wheel_size = wheel_size
+
+    # def go(self):
+    #     return "vrrrrrrrooom!"
+
+    # def fill_up_tank(self):
+    #     return "filling up!"
+
+
+    # def __init__(self, wheel_number, wheel_size):
+    #     self.wheel_size = wheel_size
+    #     self.wheel_number = wheel_number
+
+    # def go(self):
+    #     return "vrrrrrrrooom!"
+
+    # def fill_up_tank(self):
+    #     return "filling up!"
